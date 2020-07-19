176ea7379c723eac8369b1dbb43e4fd301c781d3
zhengshuyu1996 <zhengshuyu1996@163.com>
2020-07-19 23:28:38 +0800

    complete FindMeetingQuery.java & pass all tests

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,80 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashMap;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+    // If the duration is longer than a day, no options
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration())
+        return Arrays.asList();
+
+    // Collect confilct events and sort
+    List<TimeRange> confilctEvents = new ArrayList<TimeRange>();
+    for (Event event : events) {
+        for (String attendee : event.getAttendees()) {
+            if (request.getAttendees().contains(attendee)) {
+                confilctEvents.add(event.getWhen());
+                break;
+            }
+        }
+    }
+    confilctEvents.sort(TimeRange.ORDER_BY_START);
+
+    // Vacancies of the day
+    List<TimeRange> vacancies = new ArrayList();
+    vacancies.add(TimeRange.WHOLE_DAY);
+
+    // Traverse the confilct event list and split the vacancy
+    for (TimeRange eventspan : confilctEvents) {
+        for (TimeRange vacancy : vacancies) {
+            if (vacancy.equals(eventspan)) {
+                // If the event duration equals the vacancy, remove and break
+                
+                vacancies.remove(vacancy);
+                break;
+
+            } else if (eventspan.contains(vacancy)) {
+                // If the event duration contains the vacancy, remove the vacancy
+                
+                vacancies.remove(vacancy);
+
+            } else if (vacancy.contains(eventspan)) {
+                // If the vacancy contains the event duration, split the vacancy into two options (before and after the event)
+                
+                TimeRange before = TimeRange.fromStartEnd(vacancy.start(), eventspan.start(), false);
+                TimeRange after = TimeRange.fromStartEnd(eventspan.end(), vacancy.end(), false);
+                vacancies.remove(vacancy);
+                if (before.duration() > 0 && before.duration() >= request.getDuration())
+                    vacancies.add(before);
+                if (after.duration() > 0 && after.duration() >= request.getDuration())
+                    vacancies.add(after);
+                break;
+
+            } else if (vacancy.overlaps(eventspan)) {
+                // If the vacancy and the event overlaps, remove the overlap
+                
+                if (vacancy.start() <= eventspan.start()) {
+                    TimeRange before = TimeRange.fromStartEnd(vacancy.start(), eventspan.start(), false);
+                    if (before.duration() > 0 && before.duration() >= request.getDuration())
+                        vacancies.add(before);
+                } else {
+                    TimeRange after = TimeRange.fromStartEnd(eventspan.end(), vacancy.end(), false);
+                    if (after.duration() > 0 && after.duration() >= request.getDuration())
+                        vacancies.add(after);
+                }
+                vacancies.remove(vacancy);
+                
+            }
+        }
+    }
+
+    return vacancies;
   }
 }
